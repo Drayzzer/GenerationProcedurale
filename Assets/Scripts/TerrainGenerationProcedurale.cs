92a62fe581687bbd1e153913e8f6714a53782892
Jordan <j.naval@nouvellesimages.xyz>
2026-04-20 12:24:34 +0200

    fixed mesh Generation procedurale

--- Assets/Scripts/TerrainGenerationProcedurale.cs
+++ Assets/Scripts/TerrainGenerationProcedurale.cs
@@ -36,7 +36,7 @@ public class TerrainGenerationProcedurale : MonoBehaviour
             for (int x = 0; x < Zsize; x++)
             {
                 float Ypos = Mathf.PerlinNoise(x + Xoffset * noiseScale, z + Zoffset *  noiseScale) * heightMultiplier;
-                _vertices[i] = new Vector3(x, Ypos, z);
+                _vertices[i] = new Vector3(x, 0, z);
                 i++;
             }
         }
@@ -46,23 +46,19 @@ public class TerrainGenerationProcedurale : MonoBehaviour
         
         int vertex = 0;
         int triangleIndex = 0;
-
-        for (int z = 0; z < Zsize; z++)
+        
+        for (int x = 0; x < Xsize; x++)
         {
-            for (int x = 0; x < Xsize; x++)
-            {
-                triangles[triangleIndex + 0] = vertex + 0;
-                triangles[triangleIndex + 1] = vertex + Xsize + 1;
-                triangles[triangleIndex + 2] = vertex + 1;
-                
-                triangles[triangleIndex + 3] = vertex + 1;
-                triangles[triangleIndex + 4] = vertex + Xsize + 1;
-                triangles[triangleIndex + 5] = vertex + Xsize + 2;
+            triangles[triangleIndex + 0] = vertex + 0;
+            triangles[triangleIndex + 1] = vertex + Xsize + 1;
+            triangles[triangleIndex + 2] = vertex + 1;
+            
+            triangles[triangleIndex + 3] = vertex + 1;
+            triangles[triangleIndex + 4] = vertex + Xsize + 1;
+            triangles[triangleIndex + 5] = vertex + Xsize + 2;
 
-                vertex++;
-                triangleIndex += 6;
-            }
             vertex++;
+            triangleIndex += 6;
         }
         
         _mesh.Clear();
@@ -71,5 +67,4 @@ public class TerrainGenerationProcedurale : MonoBehaviour
         _mesh.triangles = triangles;
         _mesh.RecalculateNormals();
     }
-    
 }
